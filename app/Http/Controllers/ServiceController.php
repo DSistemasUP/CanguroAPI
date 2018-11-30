7b8599f9f78bc9b660fc7f0972d984bbfa4e372b
CrisTovilla <163201@ids.upchiapas.edu.mx>
2018-11-30 10:56:44 -0600

    Update ServiceController.php

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -29,66 +29,78 @@ class ServiceController extends Controller
      */
     public function getAll_Services()
     {
-        $services = Service::all();
-        $arr_services=array();      
-        foreach ($services as $service) {
-            array_push($arr_services,         
-                ['id' => $service->id,
-                 'service_type' => ServiceType::find($service->id_service_type),
-                 'user' => User::find($service->id_user),
-                 'location_a' => Location::find($service->id_location_a),
-                 'description_a' => $service->description_a,
-                 'location_b' => Location::find($service->id_location_b),
-                 'description_b' => $service->description_b,
-                 'created_at'=> $service->created_at,
-                 'updated_at'=> $service->updated_at,
-                 'date_time_required' => $service->date_time_required
-                ]
-            );
+        try{
+            $services = Service::all();
+            $arr_services=array();      
+            foreach ($services as $service) {
+                array_push($arr_services,         
+                    ['id' => $service->id,
+                    'service_type' => ServiceType::find($service->id_service_type),
+                    'user' => User::find($service->id_user),
+                    'location_a' => Location::find($service->id_location_a),
+                    'description_a' => $service->description_a,
+                    'location_b' => Location::find($service->id_location_b),
+                    'description_b' => $service->description_b,
+                    'created_at'=> $service->created_at,
+                    'updated_at'=> $service->updated_at,
+                    'date_time_required' => $service->date_time_required
+                    ]
+                );
+            }
+            return response()->json($arr_services);
+        }catch(Exception $e){
+            return response()->json([
+                'message' => 'Error'
+            ], 404);
         }
-        return response()->json($arr_services);
+        
     }
 
     public function createService(Request $request)
     {
-        $request->validate([
-            'description_a' => 'required|string',
-            'address_a' => 'required|string',
-            'description_b' => 'required|string',
-            'address_b' => 'required|string',
-            'service_type' => 'required|string',
-            'date_time_required' => 'required|string' ,
-            'email' => 'required|string'            
-        ]);
-        $LocationA=new Location([
-            'latitude' => '0',
-            'longitude' => '0',
-            'address' => $request->address_a,
-        ]);
-        $LocationB=new Location([
-            'latitude' => '0',
-            'longitude' => '0',
-            'address' => $request->address_b,
-        ]);
-        $LocationB->save();
-        $LocationA->save();
-        $date= Carbon::parse($request->date_time_required);
-        $date=$date->format('y/m/d h:i:s');
-        $user = User::select('id')->where('email', $request->email)->get();
-        $servicetype = ServiceType::where('name', $request->service_type)
-               ->get();
-        $Service=new Service([
-            'id_user'=>$user[0]->id,
-            'description_a' => $request->description_a,
-            'id_location_a' =>  $LocationA->id,
-            'description_b' => $request->description_b,
-            'id_location_b' => $LocationB->id,
-            'id_service_type' => $servicetype[0]->id,
-            'date_time_required' => $date    
-        ]);
-
-        $Service->save();
-  
+        try{
+            $request->validate([
+                'description_a' => 'required|string',
+                'address_a' => 'required|string',
+                'description_b' => 'required|string',
+                'address_b' => 'required|string',
+                'service_type' => 'required|string',
+                'date_time_required' => 'required|string' ,
+                'email' => 'required|string'            
+            ]);
+            $LocationA=new Location([
+                'latitude' => '0',
+                'longitude' => '0',
+                'address' => $request->address_a,
+            ]);
+            $LocationB=new Location([
+                'latitude' => '0',
+                'longitude' => '0',
+                'address' => $request->address_b,
+            ]);
+           
+            $date= Carbon::parse($request->date_time_required);
+            $date=$date->format('y/m/d h:i:s');
+            $user = User::select('id')->where('email', $request->email)->get();
+            $servicetype = ServiceType::where('name', $request->service_type)
+                   ->get();
+            $LocationB->save();
+            $LocationA->save();
+            $Service=new Service([
+                'id_user'=>$user[0]->id,
+                'description_a' => $request->description_a,
+                'id_location_a' =>  $LocationA->id,
+                'description_b' => $request->description_b,
+                'id_location_b' => $LocationB->id,
+                'id_service_type' => $servicetype[0]->id,
+                'date_time_required' => $date    
+            ]);  
+            $Service->save();
+        }catch(Exception $e){
+            return response()->json([
+                'message' => 'Error'
+            ], 404);
+        }
     }
 
 
